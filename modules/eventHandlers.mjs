f1f407866d6cc734b829d491732ef83c8bd49e22
Carl Anthon Wellsjö <58119759+anthonwellsjo@users.noreply.github.com>
2020-10-13 08:59:35 +0200

    Första build. (#30)
    * First commit, created fresh project
    
    * Byggde HTML strukturen
    
    Vi har gjort en vy för brädan.
    
    Vi har skapat två JS moduler
    
    * Har lagt html-kod för startsidan
    
    Ändrat typsnitt
    
    Gjort om sidans header
    
    Lagt till css-kod
    
    * Update script.js
    
    kommenterar ut kallet till login-page så länge. perfekt jobb. för den som behöver jobba på login-page får se till att den renderas själv.
    
    * Add cards *work in progress
    
    Lagt till event-lyssnare för alla kolumnet för
    lägg-till-nytt-kort-knappar
    
    *Work in progress
    
    * Add cards färdig.
    
    Ett card läggs till när man trycker på knappen. Notera följande:
    1. Kortet som läggs till är strukturerat på samma sätt som de kort som
       finns sedan innan MEN har även en klass "card-description" på <p>.
    2. Har skapat en modul "eventHandlers.mjs". Här lagras följande:
    	1a. Alla eventhandlers (ex. onClickEventHandler...)
    	1b. Alla funktioner som lägger till eventhandlers i DOM (ex.
    	addEventListener("click", onClickEventHandler).
    
    /Anthon
    
    * Ta bort kort *work in progress
    
    *Har justerat add-card-funktionen så att den lägger till en ta-bort-knapp
    i varje card.
    *Har delat upp koden så att funktionerna addcard (och snart deleteCard)
    ligger i page-modulen.
    *Har anpassat CSS-koden i Card för att kunna flytta min delete-card-btn
    till övre höger hörna ====> .card har nu position: absolute. Jag kan
    inte se några förändringar/buggar som detta kan ha medfört.
    
    /Anthon
    
    * Ta bort cards färdig*.
    
    *Användaren kan ta bort kort.
    *Eventhandler onDeleteCardClickEvetHandler i modulen eventHandlers.
    *addOnDeleteCardClickEventHandler i modulen eventHandlers.
    *Har gjort så mycket abstraktion som möjligt för att kunna
    navigera och bygga på koden så lätt som möjligt.
    
    * redigera kort inlagt
    
    lagt till 'edit' knapp på korten
    
    eventlisteners för editknapparna i evenHandlers
    
    * Bytt namn på den classen float-left till column-container
    
    * Flyttade kortredigering till en egen funktion i page som kallas i eventhandlers
    
    när man klickar edit så blir det automatiskt fokus på rutan som ska redigeras
    
    * Nu kan man bara redigera ett kort i taget
    
    * Snabbt ansiktslyft på edit-knapp
    
    *Flyttat knapp till höger och förmniskat fontsize + bold \n *Har ändrat .class till .setAttribute('class', 'edit-done-button' eftersom det inte fann någon classlist att modifiera.
    
    * Fixade cursor: pointer på lägg till card-knapp
    
    * addade login funktionalitet samt json fil
    
    * login funktionalitet och json fil
    
    La till event handler till logga in knappen. Knappen hänvisar nu till Kanban vid lyckad inloggning. Skapade alla användare i en JSON fil. Skapade login authentication funktion.
    
    * Ändrat höjden på main samt mindre justering av login-container
    
    Skapat knapp i headern  för att logga in
    
    * Ändrat höjden på main samt mindre justering av login-container
    
    Lagt till kanpp för att logga ut i headern
    
    * Tagit bort knappen i html-koden på startsidan
    
    Co-authored-by: Marie Eriksson <kerstinmarie@hotmail.com>
    Co-authored-by: Jonas Yzermans <jonazyzermans@gmail.com>
    Co-authored-by: Marie <42992979+kerstinmarie@users.noreply.github.com>
    Co-authored-by: Criss Alvianto <criss.alvianto@hotmail.com>

---
+++ modules/eventHandlers.mjs
@@ -0,0 +1,54 @@
+/* 
+
+Här finns alla funktioner som:
+
+1. Lägger till event handlers till element.
+2. Är event handlers.
+
+/Anthon
+
+*/
+
+import { page } from './page.mjs';
+import { data } from './data.mjs';
+
+export const eventHandlers = {
+    addOnAddCardBtnClickEventHandlers: function () {
+        console.log("add card");
+        const addCardBtns = document.getElementsByClassName("add-card-button");
+        Object.keys(addCardBtns).forEach(key => {
+            addCardBtns[key].addEventListener("click", e => eventHandlers.onAddCardClickEventHandler(e))
+       //     console.log(addCardBtns[key]);
+        });
+    },
+    addOnDeleteCardClickEventHandler: function (button) {
+        button.addEventListener("click", e => eventHandlers.onDeleteCardClickEventHandler(e))
+    },
+    onAddCardClickEventHandler: function (e) {
+        page.addCard(e);
+    },
+    onDeleteCardClickEventHandler: function (e) {
+        page.deleteCard(e);
+    },
+    addEditCardClickEventHandlers: function () {
+        const editCardsButtons = document.getElementsByClassName("edit-card-button");
+        Object.keys(editCardsButtons).forEach(key => {
+            editCardsButtons[key].addEventListener("click", e => eventHandlers.onEditCardClickEventHandler(e))
+        });
+    },
+    addEditCardEventHandler: function (button)
+    {
+        button.addEventListener("click", e => eventHandlers.onEditCardClickEventHandler(e));
+    },
+    onEditCardClickEventHandler: function (e) {
+        page.editCard(e);
+    },
+
+    addOnLoginBtnClickEventHandler: function () {
+        const loginBtn = document.getElementById("login-button");
+
+        loginBtn.addEventListener("click", () => {
+            data.loginAuthentication();
+        })
+    }
+}
---
+++ modules/page.mjs
@@ -0,0 +1,130 @@
+import { eventHandlers } from "./eventHandlers.mjs";
+
+export const page = {
+    getFrontPage: function () {
+        return `
+        <header class="page-header">
+            <div class="flex-container">
+                <div class="empty-keeper"></div>
+                <div class="logo-keeper"><h2 class="logotext">Kanban bräda</h2></div>
+                <div class="button-keeper"></div>
+            </div>
+        </header>
+        <main>
+            <div class="main">
+                <div class="login-container">
+                    <header>
+                        <h1>Logga in</h1>
+                    </header>
+                        <label for="username">Användarnamn: </label>
+                        <input id="username" name="username" type="text" placeholder="Användarnamn">
+                        <label for="username">Lösenord: </label>
+                        <input id="password" name="password" type="password" placeholder="Lösenord">
+                        <button id="login-button">Logga in</button> 
+                </div>
+            </div>    
+        </main>
+   <footer class="footer"></footer>
+   `
+    },
+    getBoardPage: function () {
+        return `
+    <header class="page-header">
+        <div class="flex-container">
+            <div class="empty-keeper"></div>
+            <div class="logo-keeper"><h2 class="logotext">Kanban bräda</h2></div>
+            <div class="button-keeper"><button id="logout-button">Logga ut</button></div>
+        </div>
+    </header>
+    <main>
+        <div class="main">
+            <div class="column-container">
+                <div id="todo-column" class="column">
+                    <header class="column-header">
+                        <h3>Todo</h3>
+                    </header>
+                    <div class="enter-card">
+                        <button class="add-card-button">+ Lägg till ett kort</button>
+                    </div>
+                </div>
+                <div id="doing-column" class="column">
+                    <header class="column-header">
+                        <h3>Doing</h3>
+                    </header>
+                    <div class="enter-card">
+                        <button class="add-card-button">+ Lägg till ett kort</button>
+                    </div>
+                </div>
+                <div id="test-column" class="column">
+                    <header class="column-header">
+                        <h3>Test</h3>
+                    </header>
+                    <div class="enter-card">
+                        <button class="add-card-button">+ Lägg till ett kort</button>
+                    </div>
+                   
+                </div>
+                <div id="done-column" class="column">
+                    <header class="column-header">
+                        <h3>Done</h3>
+                    </header>
+                    <div class="enter-card">
+                        <button class="add-card-button">+ Lägg till ett kort</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="footer">
+    </footer>
+`
+    },
+    addCard: function (e) {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const button = document.createElement("button");
+        button.setAttribute("class", "delete-card-btn");
+        button.innerText = "X";
+        eventHandlers.addOnDeleteCardClickEventHandler(button);
+
+        const editButton = document.createElement("button");
+        editButton.setAttribute("class", "edit-card-button");
+        editButton.innerText = "Edit";
+        eventHandlers.addEditCardEventHandler(editButton);
+
+        const par = document.createElement("p");
+        par.setAttribute("class", "card-description");
+        par.innerText = `
+        Nytt kort. ID: ${new Date().getTime()}
+        `;
+        card.append(button, par, editButton);
+        e.target.parentNode.append(card);
+    },
+    deleteCard: function(e) {
+        e.target.parentNode.remove();
+    },
+    editCard: function(e)
+    {
+        if(page.cardIsBeingEdited == false)
+        {
+            e.target.style.display = "none";
+            let parentText = e.target.parentNode.getElementsByClassName("card-description")[0];
+            parentText.contentEditable = true;
+            let btn = document.createElement("button");
+            btn.setAttribute("class", "edit-done-button");
+            btn.innerHTML = "Done";
+            e.target.parentNode.append(btn);
+            parentText.focus();
+            btn.addEventListener("click", function()
+            {
+                parentText.contentEditable = false;
+                e.target.parentNode.removeChild(btn);
+                e.target.style.display = "block";
+                page.cardIsBeingEdited = false;
+            });
+            page.cardIsBeingEdited = true;
+        }
+    },
+    cardIsBeingEdited: new Boolean(false)
+}
